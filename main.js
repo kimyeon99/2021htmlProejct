bf6c409d5a94e0b6290eac23290495c48d313ca0
kimyeon99 <tmdwo7017@naver.com>
2021-07-10 19:44:11 +0900

    2021-07-05~07-10 project

---
+++ main.js
@@ -0,0 +1,42 @@
+function box2_click(){
+    window.open('http://www.naver.com/');
+}
+function box3_click(){
+    window.open('http://www.naver.com/');
+}
+function box4_click(){
+    window.open('http://www.naver.com/');
+}
+function box5_click(){
+    window.open('http://www.naver.com/');
+}
+function box6_click(){
+    window.open('http://www.naver.com/');
+}
+function box7_click(){
+    window.open('http://www.naver.com/');
+}
+function box8_click(){
+    window.open('http://www.naver.com/');
+}
+function box9_click(){
+    window.open('http://www.naver.com/');
+}
+function box10_click(){
+    window.open('http://www.naver.com/');
+}
+function box11_click(){
+    window.open('http://www.naver.com/');
+}
+function box12_click(){
+    window.open('http://www.naver.com/');
+}
+function box13_click(){
+    window.open('http://www.naver.com/');
+}
+
+
+var box = document.gettElementById('box2');
+box.addEventListener('mouseover', function(){
+    window.open('http://www.naver.com/');
+})
